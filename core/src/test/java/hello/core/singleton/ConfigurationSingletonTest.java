a5316e2b3e4b07243076d12103221d2e3a1f6f5d
HS <probability@kakao.com>
2025-11-13 21:21:45 +0900

    @Configuration 내용 공부

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -50,6 +50,25 @@ public class ConfigurationSingletonTest {
           */
     }
 
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
 
+        System.out.println("Bean : " + bean.getClass());
+        /*
+         * 결과물
+         * Bean : class hello.core.AppConfig$$EnhancerBySpringCGLIB$$ee4ff54b
+         * 
+         * 예상되는 결과 값은 class hello.core.AppConfig 
+         * 
+         *  다른 이유는 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 이용하여 AppConfig 클래스를 상속받은
+         * 임의의 다른 클래스로 만들고, 그 다른 클래스를 스프링 빈으로 등록 한 것이다. 
+         * 
+         * 만약 AppConfig 에 @Configuration 애노테이션을 제거 하면 예상되는 결과값이 나옴
+         * -> configurationTest() 테스트에서의 예상되는 결과값이 나오게됨.
+         * -> 위의 테스트가 오류가 날것 이유는 각 객체가 서로 다른 객체가 생성되기 때문에.
+         */
+    }
 
 }
